15f0ffb89abd1915846e6a0ab9dd5279c78a6d2b
Jacktone Clarance <77950647+claranceJ@users.noreply.github.com>
2023-04-09 14:33:54 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+while True:
+  animal=input("What animal do you want? ")
+  if animal=="Cow" or animal=="cow":
+    print("A cow goes moo")
+  else:
+    print("Ummm...the Lesser Spotter Lemur goes awooga.")
+  choice=input("Do you want to exit?")
+  if choice =="yes" or choice=="y":
+    break
+
+
+   
